b8bca540587e4c70e6af43a1d9044f75f24f50f4
pen <iamapen@studiopoppy.com>
2025-04-15 01:01:37 +0900

    5.6: ビジネスルールに関する妥当性確認
    Entity, ValueObject でできなければドメインサービス

--- src/Application/Domain/Service/SendMoneyService.php
+++ src/Application/Domain/Service/SendMoneyService.php
@@ -19,6 +19,9 @@ class SendMoneyService implements SendMoneyUseCase
     public function sendMoney(SendMoneyCommand $command): bool
     {
         // TODO ビジネスルールに関する妥当性確認を行う
+        // 送金元の口座が存在するか？
+        // 送金先の口座が存在するか？
+
         // TODO モデルの状態を変える
         // TODO 処理結果を返す
         return true;
